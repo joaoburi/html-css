7d7f377ceb5b84b462b4cfd0fd8a439aa39c96ee
joaoburi <joaoburi64@gmail.com>
2024-04-22 17:11:43 -0300

    att

---
+++ exercicios/ateste/script.js
@@ -0,0 +1,10 @@
+
+function mostrar() {
+    document.getElementById("conteudo").innerHTML = "<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore earum eum amet nemo, quos vitae optio repellendus facere excepturi modi repellat! Non ex unde, asperiores laboriosam voluptate officiis veniam reiciendis.</p>";
+}
+
+function aparecer() {
+    document.getElementById("foto").innerHTML = "<img src='foto-teste.jpeg'>";
+
+}
+
